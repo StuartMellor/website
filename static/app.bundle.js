cf9d1972f4604de47d9335cbbc89f3c37c64f9ef
StuartMellor <stuart_m_mellor@hotmail.com>
2019-05-15 12:38:56 +0100

    Fixed Automation

--- static/app.bundle.js
+++ static/app.bundle.js
@@ -125,7 +125,7 @@
 	            return React.createElement(
 	                'div',
 	                { style: { margin: '20% 0 0 0 ', width: '100%', height: '100%', fontFamily: 'Archivo Narrow', fontWeight: '700', fontSize: '150px', textAlign: 'center' } },
-	                'Site under construction! Check back soon... Email: stuart_m_mellor@hotmail.com'
+	                'Site under construction! Check back soon..... Email: stuart_m_mellor@hotmail.com'
 	            );
 	        }
 	    }]);
