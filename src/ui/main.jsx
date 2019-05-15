c402d96496af1dc7e27ac59f311616e9795031ed
StuartMellor <stuart_m_mellor@hotmail.com>
2019-05-15 12:23:08 +0100

    testing automation

--- src/ui/main.jsx
+++ src/ui/main.jsx
@@ -14,7 +14,7 @@ export default class Main extends React.Component {
         return(
             <div style={{margin: '20% 0 0 0 ', width: '100%', height: '100%', fontFamily: 'Archivo Narrow', fontWeight: '700', fontSize: '150px', textAlign: 'center'}}>
                     Site under construction!
-                    Check back soon....
+                    Check back soon.....
                     
                     Email: stuart_m_mellor@hotmail.com
                     
--- static/ui/main.js
+++ static/ui/main.js
@@ -13,7 +13,7 @@ class Main extends React.Component {
         return React.createElement(
             'div',
             { style: { margin: '20% 0 0 0 ', width: '100%', height: '100%', fontFamily: 'Archivo Narrow', fontWeight: '700', fontSize: '150px', textAlign: 'center' } },
-            'Site under construction! Check back soon.... Email: stuart_m_mellor@hotmail.com'
+            'Site under construction! Check back soon..... Email: stuart_m_mellor@hotmail.com'
         );
     }
 
