ef52ce5535323d56e73d6a16307919a2db6c535c
StuartMellor <stuart_m_mellor@hotmail.com>
2019-05-16 23:40:46 +0100

    Automation Working!

--- src/ui/main.jsx
+++ src/ui/main.jsx
@@ -12,7 +12,7 @@ export default class Main extends React.Component {
 
     render() {
         return(
-            <div style={{margin: '20% 0 0 0 ', width: '100%', height: '100%', fontFamily: 'Archivo Narrow', fontWeight: '700', fontSize: '150px', textAlign: 'center'}}>
+            <div style={{margin: '20% 0 0 0 ', width: '100%', height: '100%', fontFamily: 'Archivo Narrow', fontWeight: '700', fontSize: '75px', textAlign: 'center'}}>
                     Site under construction!
                     Check back soon....
                     
--- static/ui/main.js
+++ static/ui/main.js
@@ -12,7 +12,7 @@ class Main extends React.Component {
     render() {
         return React.createElement(
             'div',
-            { style: { margin: '20% 0 0 0 ', width: '100%', height: '100%', fontFamily: 'Archivo Narrow', fontWeight: '700', fontSize: '150px', textAlign: 'center' } },
+            { style: { margin: '20% 0 0 0 ', width: '100%', height: '100%', fontFamily: 'Archivo Narrow', fontWeight: '700', fontSize: '75px', textAlign: 'center' } },
             'Site under construction! Check back soon.... Email: stuart_m_mellor@hotmail.com'
         );
     }
